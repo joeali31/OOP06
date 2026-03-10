cf785193e2b2a39bc8c105f58fa4589585daeae8
joeali31 <ya9415565@gmail.com>
2026-03-10 15:46:47 -0700

    Question01

--- OOP06/Program.cs
+++ OOP06/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 : Theoretical Questions
+            /* Q1
+             * Abstraction means hiding complex implementation details and showing only the essential features of an object.
+             * Abstraction : Hides complex implementation
+             * Encapsulation : Hides internal data
+             */
+
+            /* Q1
+             * Abstract class: A class can inherit only one abstract class , Can contain implemented methods , Can have fields, constructors, and properties , Members can have access modifiers
+             * Interface : A class can implement multiple interfaces , Usually contains method signatures only , Cannot have fields or constructors , Members are usually public by default
+             */
+            #endregion
         }
     }
 }
